87e8e30fb2ade222b51c1960797f4cab838d2cf8
alatos2 <alabitosin60@gmail.com>
2020-04-14 06:13:39 +0100

    update repo for challenge 5

--- src/route.js
+++ src/route.js
@@ -11,85 +11,89 @@ const route = express.Router();
 route.post('/', (req, res) => {
   res.set('Content-Type', 'application/json');
   const {
-    name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation, periodType,
-    timeToElapse, reportedCases, population, totalHospitalBeds
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
   } = req.body;
+  const estimates = covid19ImpactEstimator({
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  });
 
-  const inputData = {
-    region: {
-      name: name,
-      avgAge: avgAge,
-      avgDailyIncomeInUSD: avgDailyIncomeInUSD,
-      avgDailyIncomePopulation: avgDailyIncomePopulation
-    },
-    periodType: periodType,
-    timeToElapse: timeToElapse,
-    reportedCases: reportedCases,
-    population: population,
-    totalHospitalBeds: totalHospitalBeds
-  };
-
-  res.status(200).json(covid19ImpactEstimator(inputData));
+  res.status(200).json(estimates);
 });
 
 route.post('/json', (req, res) => {
   res.set('Content-Type', 'application/json');
   const {
-    name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation, periodType,
-    timeToElapse, reportedCases, population, totalHospitalBeds
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
   } = req.body;
+  const estimates = covid19ImpactEstimator({
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  });
 
-  const inputData = {
-    region: {
-      name: name,
-      avgAge: avgAge,
-      avgDailyIncomeInUSD: avgDailyIncomeInUSD,
-      avgDailyIncomePopulation: avgDailyIncomePopulation
-    },
-    periodType: periodType,
-    timeToElapse: timeToElapse,
-    reportedCases: reportedCases,
-    population: population,
-    totalHospitalBeds: totalHospitalBeds
-  };
-
-  // res.status(200).json(covid19ImpactEstimator(inputData));
-  res.status(200).json(covid19ImpactEstimator(inputData));
+  res.status(200).json(estimates);
 });
 
 
 route.post('/xml', (req, res) => {
   res.set('Content-Type', 'application/xml');
   const {
-    name, avgAge, avgDailyIncomeInUSD, avgDailyIncomePopulation, periodType,
-    timeToElapse, reportedCases, population, totalHospitalBeds
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
   } = req.body;
-
-  const inputData = {
-    region: {
-      name: name,
-      avgAge: avgAge,
-      avgDailyIncomeInUSD: avgDailyIncomeInUSD,
-      avgDailyIncomePopulation: avgDailyIncomePopulation
-    },
-    periodType: periodType,
-    timeToElapse: timeToElapse,
-    reportedCases: reportedCases,
-    population: population,
-    totalHospitalBeds: totalHospitalBeds
-  };
+  const estimates = covid19ImpactEstimator({
+    region,
+    periodType,
+    timeToElapse,
+    reportedCases,
+    population,
+    totalHospitalBeds
+  });
 
   res.send(o2x({
     '?xml version="1.0" encoding="utf-8"?': null,
-    ...covid19ImpactEstimator(inputData)
+    ...estimates
   }));
 });
 
-route.get('/logs', (req, res) => {
-  const filePath = path.join(__dirname, 'access.log');
-  const contents = fs.readFileSync(filePath);
-  res.send(contents);
+// route.get('/logs', (req, res) => {
+//   const filePath = path.join(__dirname, 'access.log');
+//   const contents = fs.readFileSync(filePath);
+//   res.send(contents);
+// });
+
+
+route.get('/logs', async (req, res) => {
+  fs.readFile(path.join(__dirname, 'access.log'), (err, content) => {
+    if (err) {
+      // console.log(err);
+      return;
+    }
+    res.header('Content-Type', 'text/plain; charset=UTF-8');
+    res.status(200).send(content);
+  });
 });
 
-// export default route;
 module.exports = route;
